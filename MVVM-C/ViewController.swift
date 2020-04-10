f8213b9989ee877df8d51af6710ef654c434a578
ttdp <tiantong.dev@gmail.com>
2020-04-10 10:56:28 +0800

    add mvvm-c

---
+++ MVVM-C/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Tian Tong on 2020/4/10.
+//  Copyright © 2020 TTDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
